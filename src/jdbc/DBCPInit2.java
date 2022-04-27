a7e918c32f3b2557304499191121803cb1710ff6
sinnotech <sinnotech@DESKTOP-QFTLPTP>
2022-04-27 15:06:16 +0900

    20220427_Chap17_WebServlet

---
+++ src/jdbc/DBCPInit2.java
@@ -0,0 +1,70 @@
+package jdbc;
+
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDriver;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+public class DBCPInit2 extends HttpServlet {
+
+   @Override
+   public void init() throws ServletException {
+      loadJDBCDriver();
+      initConnection();
+
+   }
+
+   private void loadJDBCDriver() {
+	   String driverClass = getInitParameter("jdbcdriver");
+	   
+	   try {
+         Class.forName(driverClass);
+
+      } catch (ClassNotFoundException e) {
+         throw new RuntimeException("fail to load JDBC Driver", e);
+      }
+   }
+
+   private void initConnection() {
+      try {
+         String jdbcUrl = getInitParameter("jdbcUrl");
+         String username = getInitParameter("dbUser");
+         String pw = getInitParameter("dbPass");
+         ConnectionFactory connFactory = 
+        		 new DriverManagerConnectionFactory(jdbcUrl, username, pw);
+         
+         PoolableConnectionFactory poolableConnFactroy = 
+        		 new PoolableConnectionFactory(connFactory, null);
+         poolableConnFactroy.setValidationQuery("select 1");
+         
+         GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+         poolConfig.setTimeBetweenEvictionRunsMillis(1000L * 60L * 5L);
+         poolConfig.setTestWhileIdle(true);
+         poolConfig.setMinIdle(4);
+         poolConfig.setMaxTotal(50);
+
+         GenericObjectPool<PoolableConnection> connentionPool = 
+        		 new GenericObjectPool<PoolableConnection>(poolableConnFactroy, poolConfig);
+         poolableConnFactroy.setPool(connentionPool);
+
+         Class.forName("org.apache.commons.dbcp2.PoolingDriver");
+         PoolingDriver driver = (PoolingDriver) 
+        		 DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+         String poolName = getInitParameter("poolName");
+         driver.registerPool(poolName, connentionPool);
+
+      } catch (Exception e) {
+         throw new RuntimeException();
+      }
+
+   }
+
+}
